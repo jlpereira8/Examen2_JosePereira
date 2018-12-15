780cb43755288b4e0643e44b61458063e314d712
jlpereira8 <jlpereira355@gmail.com>
2018-12-14 20:48:44 -0600

    prueba commits

---
+++ cmake-build-debug/CMakeFiles/Examen2_JosePereira.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jlpereira/Música/Examen2_JosePereira/Node.cpp" "/home/jlpereira/Música/Examen2_JosePereira/cmake-build-debug/CMakeFiles/Examen2_JosePereira.dir/Node.cpp.o"
+  "/home/jlpereira/Música/Examen2_JosePereira/Princi.cpp" "/home/jlpereira/Música/Examen2_JosePereira/cmake-build-debug/CMakeFiles/Examen2_JosePereira.dir/Princi.cpp.o"
+  "/home/jlpereira/Música/Examen2_JosePereira/Usuario.cpp" "/home/jlpereira/Música/Examen2_JosePereira/cmake-build-debug/CMakeFiles/Examen2_JosePereira.dir/Usuario.cpp.o"
+  "/home/jlpereira/Música/Examen2_JosePereira/main.cpp" "/home/jlpereira/Música/Examen2_JosePereira/cmake-build-debug/CMakeFiles/Examen2_JosePereira.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Examen2_JosePereira.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Examen2_JosePereira.dir/main.cpp.o"
+  "CMakeFiles/Examen2_JosePereira.dir/Usuario.cpp.o"
+  "CMakeFiles/Examen2_JosePereira.dir/Node.cpp.o"
+  "CMakeFiles/Examen2_JosePereira.dir/Princi.cpp.o"
+  "Examen2_JosePereira.pdb"
+  "Examen2_JosePereira"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Examen2_JosePereira.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
